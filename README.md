5164338bfd59bed5aa2e2db348a8b09917ecd37d
yen3 <yen3rc@gmail.com>
2018-02-26 17:50:02 +0800

    Refine clone

---
+++ README.md
@@ -0,0 +1,6 @@
+# Build
+
+```
+./clone.sh
+python setup.py build
+```
--- clone.sh
+++ clone.sh
@@ -6,6 +6,12 @@ ORIGIN_DIR=${TOP_DIR}/origin
 
 set -ex
 
+rm -rf ${TOP_DIR}/bin         \
+       ${TOP_DIR}/virtManager \
+       ${TOP_DIR}/virtcli     \
+       ${TOP_DIR}/virtconv    \
+       ${TOP_DIR}/virtinst
+
 cd origin
 if [ ! -f ${ORIGIN_DIR}/virt-manager ]; then
     git submodule init
